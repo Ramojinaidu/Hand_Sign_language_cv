07dc9ab9bd1efbc7a1348af7b760e9e57ac49d9b
ramoji-naidu <ramoji192358@gmail.com>
2025-02-28 22:31:05 +0530

    updated hand_detector code structure

--- main.py
+++ main.py
@@ -4,20 +4,17 @@ import hand_detector
 
 frame = cv.VideoCapture(0)
 
-
 detector = hand_detector.Hand_detector()
 while True:
     ret, img = frame.read()
-    #print(img.dtype)
     k = cv.waitKey(1)
     if k == ord("q"):
         break
 
+    detector.draw_detection(img)
+    detector.draw_bbox()
 
-    img_l = detector.draw_detection(img,True,True)
-    img_f= cv.flip(img_l, 1)
+    cv.imshow("video", detector.get_detected_image())
 
-    cv.imshow("video",img_f)
-   
 frame.release()
 cv.destroyAllWindows()
